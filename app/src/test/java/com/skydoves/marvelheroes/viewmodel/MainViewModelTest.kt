6c7b5e313c541009be467230d80da8346b27a668
skydoves <skydoves@naver.com>
2020-04-14 22:59:47 +0900

    UnitTest MainViewModelTest and PosterDetailViewModelTest

---
+++ app/src/test/java/com/skydoves/marvelheroes/viewmodel/MainViewModelTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.marvelheroes.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.atLeastOnce
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.skydoves.marvelheroes.MainCoroutinesRule
+import com.skydoves.marvelheroes.model.Poster
+import com.skydoves.marvelheroes.network.MarvelClient
+import com.skydoves.marvelheroes.network.MarvelService
+import com.skydoves.marvelheroes.persistence.PosterDao
+import com.skydoves.marvelheroes.repository.MainRepository
+import com.skydoves.marvelheroes.utils.MockTestUtil
+import com.skydoves.marvelheroes.view.ui.main.MainViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class MainViewModelTest {
+
+  private lateinit var viewModel: MainViewModel
+  private lateinit var mainRepository: MainRepository
+  private val marvelService: MarvelService = mock()
+  private val marvelClient: MarvelClient = MarvelClient(marvelService)
+  private val posterDao: PosterDao = mock()
+
+  @ExperimentalCoroutinesApi
+  @get:Rule
+  var coroutinesRule = MainCoroutinesRule()
+
+  @get:Rule
+  var instantExecutorRule = InstantTaskExecutorRule()
+
+  @ExperimentalCoroutinesApi
+  @Before
+  fun setup() {
+    mainRepository = MainRepository(marvelClient, posterDao)
+    viewModel = MainViewModel(mainRepository)
+  }
+
+  @Test
+  fun fetchMarvelPosterListTest() = runBlocking {
+    val mockData = MockTestUtil.mockPosterList()
+    whenever(posterDao.getPosterList()).thenReturn(mockData)
+
+    val fetchedData = mainRepository.loadMarvelPosters { }
+    val observer: Observer<List<Poster>> = mock()
+    fetchedData.observeForever(observer)
+
+    viewModel.fetchMarvelPosterList()
+
+    verify(posterDao, atLeastOnce()).getPosterList()
+    verify(observer).onChanged(mockData)
+    fetchedData.removeObserver(observer)
+  }
+}
