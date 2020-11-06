22af2d3dd567a38a76357b21069a76d6071fd92f
skydoves <skydoves@naver.com>
2020-11-07 02:29:47 +0900

    Add a bundler dependency and refactor codes using bundle & intentOf expressions

--- app/src/main/java/com/skydoves/marvelheroes/view/ui/details/PosterDetailActivity.kt
+++ app/src/main/java/com/skydoves/marvelheroes/view/ui/details/PosterDetailActivity.kt
@@ -17,8 +17,9 @@
 package com.skydoves.marvelheroes.view.ui.details
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
+import com.skydoves.bundler.bundle
+import com.skydoves.bundler.intentOf
 import com.skydoves.marvelheroes.R
 import com.skydoves.marvelheroes.base.DatabindingActivity
 import com.skydoves.marvelheroes.databinding.ActivityPosterDetailBinding
@@ -32,30 +33,28 @@ import org.koin.android.viewmodel.ext.android.getViewModel
 class PosterDetailActivity : DatabindingActivity() {
 
   private val binding: ActivityPosterDetailBinding by binding(R.layout.activity_poster_detail)
+  private val posterId: Long by bundle(EXTRA_POSTER_ID, 0)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     onTransformationEndContainerApplyParams()
     super.onCreate(savedInstanceState)
-    val poster =
-      getViewModel<PosterDetailViewModel>().getPoster(intent.getLongExtra(posterId, 0))
     binding.apply {
+      poster = getViewModel<PosterDetailViewModel>().getPoster(posterId)
       lifecycleOwner = this@PosterDetailActivity
       activity = this@PosterDetailActivity
       adapter = PosterSeriesAdapter(plot)
-      veil = posterVeil
-      this.poster = poster
     }
   }
 
   companion object {
-    private const val posterId = "posterId"
+    private const val EXTRA_POSTER_ID = "EXTRA_POSTER_ID"
+
     fun startActivity(
       context: Context,
       transformationLayout: TransformationLayout,
       poster: Poster
-    ) {
-      val intent = Intent(context, PosterDetailActivity::class.java)
-      intent.putExtra(posterId, poster.id)
+    ) = context.intentOf<PosterDetailActivity> {
+      putExtra(EXTRA_POSTER_ID to poster.id)
       TransformationCompat.startActivity(transformationLayout, intent)
     }
   }
